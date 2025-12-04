4b04634f0fb58705c9a80020282cd78bec5b2169
suncoastdc <l.lowell@suncoastdental.org>
2025-12-04 14:03:40 -0500

    Add Electron packaging scripts and entrypoint

---
+++ electron/main.js
@@ -0,0 +1,35 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  if (isDev) {
+    win.loadURL(process.env.ELECTRON_START_URL || 'http://localhost:3000');
+    win.webContents.openDevTools({ mode: 'detach' });
+  } else {
+    win.loadFile(path.join(__dirname, '..', 'dist', 'index.html'));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
---
+++ electron/preload.js
@@ -0,0 +1,1 @@
+// Placeholder preload to keep Electron happy; extend if renderer needs Node APIs.
