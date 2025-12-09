d22b75ac9b6e2f5d754b08a0dd1753b74caba840
suncoastdc <l.lowell@suncoastdental.org>
2025-12-08 21:23:28 -0500

    Add Electron auto-update workflow

---
+++ main.js
@@ -0,0 +1,78 @@
+const path = require('node:path');
+const { app, BrowserWindow } = require('electron');
+const { autoUpdater } = require('electron-updater');
+
+let mainWindow;
+const isDev = !app.isPackaged;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      contextIsolation: false,
+      nodeIntegration: true
+    }
+  });
+
+  if (isDev) {
+    mainWindow.loadURL(process.env.ELECTRON_START_URL || 'http://localhost:5173');
+  } else {
+    mainWindow.loadFile(path.join(__dirname, 'dist', 'index.html'));
+  }
+
+  mainWindow.webContents.on('did-finish-load', () => {
+    if (app.isPackaged) {
+      autoUpdater.checkForUpdatesAndNotify();
+    }
+  });
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+autoUpdater.autoDownload = true;
+autoUpdater.autoInstallOnAppQuit = true;
+
+autoUpdater.on('checking-for-update', () => {
+  console.log('Checking for update...');
+});
+
+autoUpdater.on('update-available', (info) => {
+  console.log(`Update available: ${info.version}`);
+});
+
+autoUpdater.on('update-not-available', () => {
+  console.log('No updates available.');
+});
+
+autoUpdater.on('download-progress', (progressObj) => {
+  const logMessage = [
+    `Download speed: ${progressObj.bytesPerSecond}`,
+    `Downloaded ${progressObj.percent.toFixed(2)}%`,
+    `(${progressObj.transferred}/${progressObj.total})`
+  ].join(' - ');
+  console.log(logMessage);
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+  console.log(`Update downloaded: ${info.version}; quitting and installing...`);
+  autoUpdater.quitAndInstall();
+});
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
